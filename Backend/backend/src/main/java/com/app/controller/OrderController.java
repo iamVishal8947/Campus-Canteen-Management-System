54f9b52a04521962dc49dd3f6e2a5196be895d00
Abhishek Chopade <abhishekchopade3677@gmail.com>
2024-02-15 20:24:32 +0530

    Added @CrossOrigin at controller

--- Backend/backend/src/main/java/com/app/controller/OrderController.java
+++ Backend/backend/src/main/java/com/app/controller/OrderController.java
@@ -1,18 +1,20 @@
 package com.app.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.app.entities.Order;
 import com.app.entities.OrderStatus;
 import com.app.service.OrderService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/orders")
+@CrossOrigin(origins = "http://localhost:3000")
 public class OrderController {
 
     @Autowired
--- Backend/backend/src/main/java/com/app/controller/StudentController.java
+++ Backend/backend/src/main/java/com/app/controller/StudentController.java
@@ -1,20 +1,23 @@
 // StudentController.java
 package com.app.controller;
 
-import com.app.dto.StudentDTO;
-import com.app.service.StudentService;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
+import com.app.dto.StudentDTO;
+import com.app.service.StudentService;
 
 @RequestMapping(path="/student")
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class StudentController {
 
     private final StudentService studentService;
