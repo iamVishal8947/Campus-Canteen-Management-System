df82089163a15188f4c1f889ceb4d4b769bf9c78
iamVishal8947 <jadhavvishal8947@gmail.com>
2024-02-15 20:54:36 +0530

    getAllStudentsApi

---
+++ Backend/backend/src/main/java/com/app/controller/AdminController.java
@@ -0,0 +1,29 @@
+// AdminController.java
+package com.app.controller;
+
+import com.app.entities.Student;
+import com.app.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final StudentService studentService;
+
+    @Autowired
+    public AdminController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping("/students")
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+}
