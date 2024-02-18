867cf0b97440e984d7256c34e87e928193f48d04
Abhishek Chopade <abhishekchopade3677@gmail.com>
2024-02-18 09:05:58 +0530

    CartDTO is created

--- Backend/backend/src/main/java/com/app/dto/CreateOrderDTO.java
+++ Backend/backend/src/main/java/com/app/dto/CreateOrderDTO.java
@@ -1,51 +1,57 @@
 package com.app.dto;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import com.app.entities.OrderStatus;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
 import lombok.Getter;
 import lombok.Setter;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
 public class CreateOrderDTO {
-    @NotNull
-    private Long studentId;
-    
-    @NotNull
-    private Long itemId;
-    
-    @NotNull
-    private LocalDateTime time;
-    
-    @NotBlank
-    private String paymentMethod;
-    
-    @NotNull
-    @Positive
-    private Integer amount;
-    
-    @NotBlank
-    private String transactionId;
-    
-    @NotNull
-    @Positive
-    private Integer itemsServed;
-    
-    @NotNull
-    private Boolean isServed;
-    
-    @NotNull
+	
+	@JsonProperty(access = Access.READ_ONLY)
+	private Long orderId;
+
+	private LocalDateTime time;
+	
+	private Integer qty;
+	
+	private Integer amount;
+	
+	private Integer itemsServed;
+	
     private OrderStatus orderStatus;
-    
-    @NotNull
-    @Positive
-    private Integer discountPercentage;
-      
-    @NotNull
-    @Positive
-	private Integer quantity;
+	
+	private Integer discountPercentage;
+	
+	private String studentName;
+	
+	private List<CartDTO> carts;
 }
+
+//[
+// {
+//   "orderId": 0,
+//   "qty": 0,
+//   "amount": 0,
+//   "itemsServed": 0,
+//   "orderStatus": "PENDING",
+//   "student": {
+//     "name": "string",
+//}
+//     "carts": [
+//     {
+//       "cartId": 0,
+//       "orderId": 0,
+//		  "itemId":0,
+//       "qtyOrdered": 0,
+//       "netPrice": 0
+//     }
+//   ]
+// }
+//]
