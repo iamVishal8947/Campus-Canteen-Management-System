cfd4d7d8faf2b84ad03dd14363d370e9c8eb72e0
Atharva <avb.09.2023@gmail.com>
2024-02-18 10:38:47 +0530

    login form updated

--- cmsreactapp/src/pages/customer/MenuList/DailyMenu.js
+++ cmsreactapp/src/pages/customer/MenuList/DailyMenu.js
@@ -3,9 +3,12 @@ import MenuGrid from '../../../components/CustomerComponents/menu/MenuGrid'
 import Header from '../../../components/admin/common/Header'
 
 export default function DailyMenu() {
+  const current = new Date();
+  const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
   return (
     <div>
         <Header title="Daily Menu" subtitle = "Select your desired items"></Header>
+        <div> <h2 style={{textAlign:"center"}}>{date}</h2>  </div>
         <MenuGrid></MenuGrid>
     </div>
   )
